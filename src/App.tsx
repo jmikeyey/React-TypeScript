59a97929a13fd3b85998d4800294c961f380ebc4
Mayki <aw8hurtz@gmail.com>
2024-02-27 10:04:44 +0800

    Cleared

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,5 @@
 import './App.css';
 
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-age = '5';
-
-let printName: (name: string) => never;
-
-// type Person = {
-//   name: string;
-//   age?: number;
-// }
-interface Person  {
-  name: string;
-  age?: number;
-}
-
-type X = {
-  a: string;
-  b: number;
-}
-type Y =  {
-  c: string;
-  d: number;
-}
-
-let y: Y = {
-  c: "hello",
-  d: 42
-}
 function App() {
   return (
     <div className="App">
