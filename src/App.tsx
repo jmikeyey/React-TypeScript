a07869420b597f71f33a59e704bb1ab9a7e91faa
Mayki <aw8hurtz@gmail.com>
2024-02-27 09:54:17 +0800

    Extending Types

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,37 @@ let name: string;
 let age: number | string;
 let isStudent: boolean;
 let hobbies: string[];
-let role :[number, string]
+let role: [number, string];
 age = '5';
 
+let printName: (name: string) => never;
+
 // type Person = {
-//   name:string;
+//   name: string;
 //   age?: number;
 // }
-// let lotsOfPeople: Person[];
+interface Person  {
+  name: string;
+  age?: number;
+}
 
-// let person: Person = {
-//   name: "Mikey",
-// }
+type X = {
+  a: string;
+  b: number;
+}
+type Y =  {
+  c: string;
+  d: number;
+}
 
+let y: Y = {
+  c: "hello",
+  d: 42
+}
 function App() {
   return (
     <div className="App">
-      <h1>Hello World {age} </h1>
+      <h1>Hello World </h1>
     </div>
   );
 }
