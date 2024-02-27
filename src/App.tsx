3baf707c33128088418d2eff07108074e409c25b
Mayki <aw8hurtz@gmail.com>
2024-02-27 09:33:14 +0800

    Union types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css';
 
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role :[number, string]
+age = '5';
+
+// type Person = {
+//   name:string;
+//   age?: number;
+// }
+// let lotsOfPeople: Person[];
+
+// let person: Person = {
+//   name: "Mikey",
+// }
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World {age} </h1>
     </div>
   );
 }
