162c465217b0cc5dc6640a4cb75777254abeeba7
Mayki <aw8hurtz@gmail.com>
2024-02-27 18:26:24 +0800

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -19,36 +19,45 @@ const App: React.FC = () => {
     }
   }
 
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
 
-  const onDragEnd = (result:DropResult) => {
     console.log(result);
-    const {source, destination} = result;
 
-    if(!destination) return alert('no space')
-    if(destination.droppableId === source.droppableId && destination.index === source.index) return alert('same space')
+    if (!destination) {
+      return;
+    }
 
-    let add, 
-      active = todos, 
-      complete = completed
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
 
-    if(source.droppableId === 'TodosList'){
-      add=active[source.index];
+    let add;
+    let active = todos;
+    let complete = completed;
+    // Source Logic
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
       active.splice(source.index, 1);
     } else {
-        add=active[source.index];
-        active.splice(source.index, 1);
-      
+      add = complete[source.index];
+      complete.splice(source.index, 1);
     }
 
-    if(destination.droppableId === 'TodosList'){
-      active.splice(destination.index, 0, add)
+    // Destination Logic
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
     } else {
-      complete.splice(destination.index, 0, add)
+      complete.splice(destination.index, 0, add);
     }
 
-    setCompleted(complete)
-    setTodos(active)
-  }
+    setCompleted(complete);
+    setTodos(active);
+  };
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
         <div className="App">
