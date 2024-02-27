003f80e6e04346c6e74d417d368e5c7a2dfdff4b
Mayki <aw8hurtz@gmail.com>
2024-02-27 15:35:21 +0800

    No Drag and Drop yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import InputField from './components/InputField';
 import './css/App.css';
+import { Todo } from './model';
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>(""); //inputField handler
+  const [todos, setTodos] = useState<Todo[]>([]); // submitButton handler
+  const handAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
+      setTodo("");
+    }
+  }
+
+  console.log(todos)
+
   return (
     <div className="App">
         <span className="heading">Taskify</span>
+        <span className="heading">  </span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handAdd}  />
+
+        <TodoList  todos={todos}  setTodos={setTodos} />
     </div>
   );
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+import { Todo } from "../model"
+import SingleTodo from "./SingleTodo";
+import "./styles.css"
+
+interface Props{
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const TodoList: React.FC<Props> = ({todos, setTodos}) => {
+    return (
+        <div className="todos">
+            { todos.map((todo, i) => (
+                <SingleTodo todo={todo} 
+                            key={todo.id} 
+                            todos= {todos}
+                            setTodos={setTodos}
+                />
+            )) }
+        </div>
+    )
+}
+
+export default TodoList
